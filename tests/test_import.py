b4de03e08f792ff6e446b39572762db89d4ef752
Binh Vu <binh-vu@users.noreply.github.com>
2025-04-26 16:39:09 -0700

    Initial commit

---
+++ tests/test_import.py
@@ -0,0 +1,25 @@
+import pkgutil
+from importlib import import_module
+from pathlib import Path
+
+
+def test_import(ignore_deprecated: bool = True):
+    pkg = [
+        path
+        for path in Path(__file__).parent.parent.glob("*/__init__.py")
+        if path.parent.name != "tests"
+    ][0]
+
+    stack = [(pkg.__name__, pkg.absolute())]
+
+    while len(stack) > 0:
+        pkgname, pkgpath = stack.pop()
+        for m in pkgutil.iter_modules([str(pkgpath)]):
+            mname = f"{pkgname}.{m.name}"
+            if ignore_deprecated and mname.find("deprecated") != -1:
+                continue
+            if m.ispkg:
+                stack.append((mname, pkgpath / m.name))
+            import_module(mname)
+            import_module(mname)
+            import_module(mname)
