539d87e9d149c6e6c6a714c1c2d7428a15219625
Binh Vu <bvu687@gmail.com>
2025-04-27 06:17:45 -0700

    wip

---
+++ build_containers.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -ex
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+DATE=$(date +%Y%m%d)
+
+build_docker_image() {
+    local folder=$1
+    local image_name=toan2/$1
+    
+    if [[ -z "$folder" ]]; then
+        echo "Usage: build_docker_image <folder>"
+        exit 1
+    fi
+
+    cd $SCRIPT_DIR/containers/$folder
+    docker build -t "$image_name:$DATE" .
+}
+
+# Example usage
+build_docker_image manylinux_2_34_aarch64
